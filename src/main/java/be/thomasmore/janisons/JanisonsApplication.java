90cc2464c2858a6e04d73c152f988dbbab93ba32
GewoonToon <thundertonemail@gmail.com>
2021-02-17 13:34:31 +0100

    Initial commit

---
+++ src/main/java/be/thomasmore/janisons/JanisonsApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.janisons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanisonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanisonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/janisons/JanisonsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.janisons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanisonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
